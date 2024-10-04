48af60002bc77368fcddfce788234fc840bcb280
fayed <fayedhasan1000@gmail.com>
2024-10-05 00:28:50 +0600

    step 3 done

--- StudentList.java
+++ StudentList.java
@@ -13,12 +13,12 @@ public class StudentList {
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String r = s.readLine();
-                String i[] = r.split(",");
-                for (String j : i) {
+                BufferedReader bufferedReader = new BufferedReader(
+                                                new InputStreamReader(
+                                                new FileInputStream("students.txt")));
+                String readLine = bufferedReader.readLine();
+                String strings[] = readLine.split(",");
+                for (String j : strings) {
                     System.out.println(j);
                 }
             } catch (Exception e) {
@@ -27,30 +27,30 @@ public class StudentList {
         } else if (args[0].equals("r")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String r = s.readLine();
-//                System.out.println(r);
-                String i[] = r.split(",");
-                Random x = new Random();
-                int y = x.nextInt(i.length);
-                System.out.println(i[y].trim());
+                BufferedReader bufferedReader = new BufferedReader(
+                                   new InputStreamReader(
+                                   new FileInputStream("students.txt")));
+                String readLine = bufferedReader.readLine();
+//                System.out.println(readLine);
+                String strings[] = readLine.split(",");
+                Random random = new Random();
+                int studentIndex = random.nextInt(strings.length);
+                System.out.println(strings[studentIndex].trim());
             } catch (Exception e) {
             }
             System.out.println("Data Loaded.");
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
-                BufferedWriter s = new BufferedWriter(
-                        new FileWriter("students.txt", true));
-                String t = args[0].substring(1);
-                Date d = new Date();
-                String df = "dd/mm/yyyy-hh:mm:ss a";
-                DateFormat dateFormat = new SimpleDateFormat(df);
-                String fd = dateFormat.format(d);
-                s.write(", " + t + "\nList last updated on " + fd);
-                s.close();
+                BufferedWriter bufferedWriter = new BufferedWriter(
+                                   new FileWriter("students.txt", true));
+                String substring = args[0].substring(1);
+                Date date = new Date();
+                String date_formate = "dd/mm/yyyy-hh:mm:ss a";
+                DateFormat dateFormat = new SimpleDateFormat(date_formate);
+                String fd = dateFormat.format(date);
+                bufferedWriter.write(", " + substring + "\nList last updated on " + fd);
+                bufferedWriter.close();
             } catch (Exception e) {
             }
 
@@ -58,15 +58,15 @@ public class StudentList {
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String r = s.readLine();
-                String i[] = r.split(",");
+                BufferedReader bufferedReader = new BufferedReader(
+                                   new InputStreamReader(
+                                   new FileInputStream("students.txt")));
+                String readLine = bufferedReader.readLine();
+                String strings[] = readLine.split(",");
                 boolean done = false;
                 String t = args[0].substring(1);
-                for (int idx = 0; idx < i.length && !done; idx++) {
-                    if (i[idx].trim().equals(t)) {
+                for (int idx = 0; idx < strings.length && !done; idx++) {
+                    if (strings[idx].trim().equals(t)) {
                         System.out.println("We found it!");
                         done = true;
                     }
@@ -77,14 +77,14 @@ public class StudentList {
         } else if (args[0].equals("c")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream("students.txt")));
-                String D = s.readLine();
-                char a[] = D.toCharArray();
+                BufferedReader bufferedReader = new BufferedReader(
+                                   new InputStreamReader(
+                                   new FileInputStream("students.txt")));
+                String D = bufferedReader.readLine();
+                char charArray[] = D.toCharArray();
                 boolean in_word = false;
                 int count = 0;
-                for (char c : a) {
+                for (char c : charArray) {
                     if (c == ' ') {
                         if (!in_word) {
                             count++;
@@ -94,7 +94,7 @@ public class StudentList {
                         }
                     }
                 }
-                System.out.println(count + " word(s) found ");
+                System.out.println(count + " word(bufferedReader) found ");
             } catch (Exception e) {
             }
             System.out.println("Data Loaded.");
